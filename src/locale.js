a64a8145b7465253703c389acaa0f013f4369ad2
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-02 22:03:20 +0100

    Installed few packages

---
+++ src/locale.js
@@ -0,0 +1,16 @@
+export default {
+    pl: {
+        logIn: 'Zaloguj się',
+        signUp: 'Zarejestruj się',
+        'main.jumbotron.first': 'Zmierz swoje codzienne zadania',
+        'main.jumbotron.second': 'i planuj swój dzień lepiej!',
+        'main.jumbotron.button': 'Spróbuj za darmo już teraz',
+    },
+    en: {
+        logIn: 'Log In',
+        signUp: 'Sign Up',
+        'main.jumbotron.first': 'Measure your everyday tasks',
+        'main.jumbotron.second': 'and plan your day even better!',
+        'main.jumbotron.button': 'Try now for free',
+    },
+};
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+export default {
+    index: '/',
+    signUp: '/sign-up',
+    logIn: '/log-in',
+};
