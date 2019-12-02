f06f9586c9b0789ecf71b452a93e291d435db38a
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-02 19:19:22 +0100

    TypographyLogo component

---
+++ src/components/TypographyLogo/TypographyLogo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './TypographyLogo.module.scss';
+
+const TypographyLogo = () => <span className={styles.typographyLogo}>Zhonya</span>;
+
+export default TypographyLogo;
