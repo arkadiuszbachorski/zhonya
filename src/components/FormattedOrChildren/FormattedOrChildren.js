2a2b7e69c3ff21d114273913c9ff2ecb7e47c14f
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-11 19:45:16 +0100

    FormattedOrChildren component

---
+++ src/components/FormattedOrChildren/FormattedOrChildren.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+
+const FormattedOrChildren = ({ children, messageId }) => {
+    if (children) {
+        return children;
+    }
+
+    return <FormattedMessage id={messageId} />;
+};
+
+FormattedOrChildren.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.element), PropTypes.element, PropTypes.string]),
+    messageId: PropTypes.string,
+};
+
+FormattedOrChildren.defaultProps = {
+    children: null,
+    messageId: null,
+};
+
+export default FormattedOrChildren;
