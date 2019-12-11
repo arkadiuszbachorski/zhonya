24097434475f706529a00aaae6cc825d239d95eb
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-11 22:24:58 +0100

    Change hooks exports

--- src/hooks/providers/useRedirectProvider.js
+++ src/hooks/providers/useRedirectProvider.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 /*
  * To be used only once in the highest component, near RedirectContext.Provider
--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 const useToggle = (init = false) => {
     const [state, setState] = useState(init);
