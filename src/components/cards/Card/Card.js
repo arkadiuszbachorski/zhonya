810e094d957762677f5c024921dfb606f1c658f6
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-13 00:29:12 +0100

    Add inverted border prop to Card

--- src/components/cards/Card/Card.js
+++ src/components/cards/Card/Card.js
@@ -3,18 +3,22 @@ import cn from 'classnames';
 import PropTypes from 'prop-types';
 import styles from './Card.module.scss';
 
-const Card = ({ children, variant, className }) => (
-    <div className={cn(styles.card, styles[variant], className)}>{children}</div>
+const Card = ({ children, variant, className, inverted }) => (
+    <div className={cn(styles.card, styles[variant], inverted ? styles.bordersInverted : null, className)}>
+        {children}
+    </div>
 );
 
 Card.propTypes = {
     children: PropTypes.node,
     variant: PropTypes.oneOf(['primary', 'danger']),
     className: PropTypes.oneOfType([PropTypes.array, PropTypes.object, PropTypes.string]),
+    inverted: PropTypes.bool,
 };
 
 Card.defaultProps = {
     variant: 'primary',
+    inverted: false,
     className: null,
     children: null,
 };
