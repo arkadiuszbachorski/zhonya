f63fd3b9e8566b3028cfe08e2914b8c548e136e8
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-14 14:07:32 +0100

    Add Authentication header to axios when logged in

--- src/hooks/middlewares/useAuthenticatedOnly.js
+++ src/hooks/middlewares/useAuthenticatedOnly.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import useAuth from '../useAuth';
 import useRedirect from '../useRedirect';
 import routes from '../../routes';
+import axios from 'axios';
 
 const useAuthenticatedOnly = (scope = null) => {
     const [auth] = useAuth();
@@ -10,6 +11,8 @@ const useAuthenticatedOnly = (scope = null) => {
     useEffect(() => {
         if (auth.token === null || (scope && auth.scope && !auth.scope.includes(scope))) {
             redirect(routes.logIn);
+        } else {
+            axios.defaults.headers.common.Authorization = `Bearer ${auth.token}`;
         }
     }, [auth.token, auth.scope, redirect, scope]);
 };
