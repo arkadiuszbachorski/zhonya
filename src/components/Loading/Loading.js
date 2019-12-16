e0c96bfc108c28be8748581e1808d62ceb7b1e1a
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-16 12:39:24 +0100

    Loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Logo from '../logos/Logo/Logo';
+import styles from './Loading.module.scss';
+import Container from '../Container/Container';
+
+const Loading = ({ loading, children }) => {
+    if (loading) {
+        return (
+            <Container variant={['marginTopLarge', 'center']}>
+                <Logo linkClassName={styles.link} className={styles.loading} />
+            </Container>
+        );
+    }
+
+    return children;
+};
+
+Loading.propTypes = {
+    loading: PropTypes.bool,
+    children: PropTypes.node,
+};
+
+Loading.defaultProps = {
+    loading: false,
+    children: null,
+};
+
+export default Loading;
