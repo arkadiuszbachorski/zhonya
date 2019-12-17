526f2a33572978473a7578d0f84fafaa41f04293
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-17 20:20:47 +0100

    useModelTitle hook

---
+++ src/hooks/useModelTitle.js
@@ -0,0 +1,35 @@
+import useStore, { storeKeys } from './useStore';
+import useMergeableState from './useMergeableState';
+
+/*
+ * TODO: Write unit test
+ * */
+
+export const useModelTitleProvider = () => {
+    return useMergeableState({
+        tag: {
+            id: null,
+            name: null,
+        },
+    });
+};
+
+const useModelTitle = (model, id) => {
+    const [modelTitle, setModelTitle] = useStore(storeKeys.useModelTitle);
+
+    const name = modelTitle[model].id === id ? modelTitle[model].name : null;
+
+    const setModelData = (providedId, providedName) => {
+        setModelTitle({
+            [model]: {
+                id: providedId,
+                name: providedName,
+                default: modelTitle[model].default,
+            },
+        });
+    };
+
+    return [name, setModelData];
+};
+
+export default useModelTitle;
