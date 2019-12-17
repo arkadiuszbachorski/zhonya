7e363e8e24d86953a5fb8670dc76e07c5afab13c
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-17 20:22:24 +0100

    SlidingMenu - close on escape

--- src/components/SlidingMenu/SlidingMenu.js
+++ src/components/SlidingMenu/SlidingMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import cn from 'classnames';
@@ -8,13 +8,27 @@ import AccentTitle from '../typography/AccentTitle/AccentTitle';
 import TypographyLogo from '../logos/TypographyLogo/TypographyLogo';
 
 const SlidingMenu = ({ children, titleId, visible, toggle }) => {
+    useEffect(() => {
+        const close = e => {
+            const { code } = e;
+            if (code === 'Escape') {
+                toggle();
+            }
+        };
+
+        if (visible) {
+            window.addEventListener('keyup', close);
+        }
+
+        return () => window.removeEventListener('keyup', close);
+    }, [visible]);
+
     return (
         <>
             <nav className={cn(styles.main, visible ? styles.active : null)}>
                 <ButtonRoundIcon variant="blank" icon={faArrowLeft} onClick={toggle} />
                 <AccentTitle messageId={titleId} />
                 <div className={styles.content}>{children}</div>
-
                 <TypographyLogo classNames={styles.typographyLogo} />
             </nav>
             <button type="button" onClick={toggle} className={cn(styles.menuClose, visible ? styles.active : null)}>
