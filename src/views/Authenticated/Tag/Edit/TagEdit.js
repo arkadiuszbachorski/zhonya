21594ee116f673355ce182dba1006fc1f341c608
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-22 19:41:14 +0100

    TagEdit - separate Task attachments to another page

--- src/views/Authenticated/Tag/Edit/TagEdit.js
+++ src/views/Authenticated/Tag/Edit/TagEdit.js
@@ -29,14 +29,9 @@ const TagEdit = () => {
 
     const [, setName] = useModelTitle('tag', id);
 
-    /*
-     * TODO:
-     *  Assign tasks to gas
-     * */
-
     useEffect(() => {
         instance.get(api.tag.edit(id)).then(response => {
-            const { tasks, tag } = response.data;
+            const { data: tag } = response;
             setForm(nullToEmptyString(pick(tag, ['name', 'description', 'color'])));
             setName(id, tag.name);
         });
