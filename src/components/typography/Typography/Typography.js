e27ce42360e6a94bbc8f5cfe26f29c188af65f14
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-22 22:55:15 +0100

    Refactor Typography

--- src/components/typography/Typography/Typography.js
+++ src/components/typography/Typography/Typography.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import styles from './Typography.module.scss';
 import cn from 'classnames';
+import styles from './Typography.module.scss';
 
 const Typography = ({ tag, children, messageId, variants, className }) => {
     const Tag = tag;
     const variantsStyled = variants.map(item => styles[item]);
 
-    if (children) {
-        return <Tag className={cn(variantsStyled, className)}>{children}</Tag>;
-    }
+    const tagChildren = children || <FormattedMessage id={messageId} />;
 
-    return (
-        <Tag className={cn(variantsStyled, className)}>
-            <FormattedMessage id={messageId} />
-        </Tag>
-    );
+    return <Tag className={cn(variantsStyled, className)}>{tagChildren}</Tag>;
 };
 
 Typography.propTypes = {
