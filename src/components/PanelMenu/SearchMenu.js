0f9140c9d3fd301d8b5a9e5e7bb3891e2ca08ff0
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-24 11:40:25 +0100

    SearchMenu - captions

--- src/components/PanelMenu/SearchMenu.js
+++ src/components/PanelMenu/SearchMenu.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FormattedMessage, FormattedRelativeTime } from 'react-intl';
 import SlidingMenu from '../SlidingMenu/SlidingMenu';
 import Input from '../forms/Input/Input';
 import useInstanceWithErrorsAndToastsAndLoading from '../../hooks/api/useInstanceWithErrorsAndToastsAndLoading';
 import useDebouncedForm from '../../hooks/useDebouncedForm';
 import api from '../../api';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import LoadingComplete from '../loading/LoadingComplete/LoadingComplete';
 import ListCaptionAndColor from '../lists/ListCaptionAndColor/ListCaptionAndColor';
 import AccentSubtitle from '../typography/AccentSubtitle/AccentSubtitle';
 import routes from '../../routes';
 import styles from './SearchMenu.module.scss';
+import formattedRelativeTimeFromDate from '../../utils/formattedRelativeTimeCount';
 
 const prepareParams = ({ search, ...rest }) => ({
     search: search === '' ? undefined : search,
@@ -23,10 +25,6 @@ const initData = {
 };
 
 const SearchMenu = ({ toggle, active }) => {
-    /*
-     * TODO: Captions
-     * */
-
     const [instance, loading, errors] = useInstanceWithErrorsAndToastsAndLoading();
 
     const [debouncedFilters, filters, handleChange] = useDebouncedForm({
@@ -59,39 +57,56 @@ const SearchMenu = ({ toggle, active }) => {
                     onChange={handleChange}
                     errors={errors.task}
                 />
-                <LoadingComplete loading={loading} empty={tasksEmpty && tagsEmpty}>
-                    {!tagsEmpty && (
-                        <>
-                            <AccentSubtitle messageId="model.tag.plural" />
-                            <ListCaptionAndColor>
-                                {data.tags.map(tag => (
-                                    <ListCaptionAndColor.Item
-                                        key={tag.id}
-                                        to={routes.tag.edit(tag.id)}
-                                        color={tag.color}
-                                        caption={'Testowanko'}
-                                        text={tag.name}
-                                    />
-                                ))}
-                            </ListCaptionAndColor>
-                        </>
-                    )}
-                    {!tasksEmpty && (
-                        <>
-                            <AccentSubtitle messageId="model.task.plural" />
-                            <ListCaptionAndColor>
-                                {data.tasks.map(task => (
-                                    <ListCaptionAndColor.Item
-                                        key={task.id}
-                                        to={routes.task.edit(task.id)}
-                                        caption={'Testowanko'}
-                                        text={task.name}
-                                    />
-                                ))}
-                            </ListCaptionAndColor>
-                        </>
-                    )}
-                </LoadingComplete>
+                {debouncedFilters.search !== '' && (
+                    <LoadingComplete loading={loading} empty={tasksEmpty && tagsEmpty}>
+                        {!tagsEmpty && (
+                            <>
+                                <AccentSubtitle messageId="model.tag.plural" />
+                                <ListCaptionAndColor>
+                                    {data.tags.map(tag => (
+                                        <ListCaptionAndColor.Item
+                                            key={tag.id}
+                                            to={routes.tag.edit(tag.id)}
+                                            color={tag.color}
+                                            caption={
+                                                <FormattedMessage
+                                                    id="model.task.pluralCounter"
+                                                    values={{ tasks: tag.tasks_count }}
+                                                />
+                                            }
+                                            text={tag.name}
+                                        />
+                                    ))}
+                                </ListCaptionAndColor>
+                            </>
+                        )}
+                        {!tasksEmpty && (
+                            <>
+                                <AccentSubtitle messageId="model.task.plural" />
+                                <ListCaptionAndColor>
+                                    {data.tasks.map(task => (
+                                        <ListCaptionAndColor.Item
+                                            className={styles.captionLowercase}
+                                            key={task.id}
+                                            to={routes.task.edit(task.id)}
+                                            caption={
+                                                <>
+                                                    <FormattedMessage id="edited" />{' '}
+                                                    <FormattedRelativeTime
+                                                        value={formattedRelativeTimeFromDate(task.updated_at)}
+                                                        numeric="auto"
+                                                        updateIntervalInSeconds={10}
+                                                    />
+                                                </>
+                                            }
+                                            text={task.name}
+                                        />
+                                    ))}
+                                </ListCaptionAndColor>
+                            </>
+                        )}
+                    </LoadingComplete>
+                )}
             </div>
         </SlidingMenu>
     );
