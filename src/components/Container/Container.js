1152566bd680f8dd2589af8bfc059047ff804547
Arkadiusz Bachorski <quez2223@gmail.com>
2019-12-29 17:35:12 +0100

    Container - add className prop

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,14 +3,19 @@ import PropTypes from 'prop-types';
 import cn from 'classnames';
 import styles from './Container.module.scss';
 
-const Container = ({ children, variant }) => {
-    return <div className={cn(styles.container, ...variant.map(item => styles[item]))}>{children}</div>;
+const Container = ({ children, variant, className }) => {
+    return <div className={cn(styles.container, className, ...variant.map(item => styles[item]))}>{children}</div>;
 };
 
 Container.propTypes = {
     children: PropTypes.node.isRequired,
     variant: PropTypes.arrayOf(PropTypes.oneOf(['center', 'smallItems', 'filters', 'marginTopLarge', 'marginBottom']))
         .isRequired,
+    className: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.string), PropTypes.string]),
+};
+
+Container.defaultProps = {
+    className: null,
 };
 
 export default Container;
