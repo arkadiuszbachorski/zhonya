8dd641c290cede63214e80d748450e71e820d358
Arkadiusz Bachorski <quez2223@gmail.com>
2020-01-02 20:44:09 +0100

    Basic implementation

---
+++ src/components/Time/Time.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import digitalTimer from '../../utils/digitalTimer';
+import styles from './Time.module.scss';
+
+const Time = ({ time }) => {
+    const { days, hours, seconds, minutes } = digitalTimer(time);
+    return (
+        <span className={styles.time}>
+            {time >= 86400 && (
+                <>
+                    {days}
+                    &nbsp;
+                    <FormattedMessage id="timer.days" values={{ value: days }} />{' '}
+                </>
+            )}
+            {time >= 3600 && (
+                <>
+                    {hours}
+                    &nbsp;
+                    <FormattedMessage id="timer.hours" values={{ value: hours }} />{' '}
+                </>
+            )}
+            {time >= 60 && (
+                <>
+                    {minutes}
+                    &nbsp;
+                    <FormattedMessage id="timer.minutes" values={{ value: minutes }} />{' '}
+                </>
+            )}
+            {seconds}
+            &nbsp;
+            <FormattedMessage id="timer.seconds" values={{ value: seconds }} />
+        </span>
+    );
+};
+
+Time.propTypes = {
+    time: PropTypes.number.isRequired,
+};
+
+export default Time;
