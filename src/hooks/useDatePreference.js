c13f392281a3220141cfc465244d51c9e3101add
Arkadiusz Bachorski <quez2223@gmail.com>
2020-01-14 12:04:26 +0100

    useDatePreference hook

---
+++ src/hooks/useDatePreference.js
@@ -0,0 +1,22 @@
+import useStore, { storeKeys } from './useStore';
+import useStateWithLocalStorage from './useStateWithLocalStorage';
+
+export const availableDatePreferences = ['relative', 'short', 'long'];
+
+export const useDatePreferenceProvider = () => {
+    const [datePreference, setDatePreference] = useStateWithLocalStorage(storeKeys.useDatePreference, 'relative');
+
+    const setAndCheckTimePreference = preference => {
+        if (!availableDatePreferences.includes(preference)) {
+            throw new Error(`Date preference has to be one of following: ${availableDatePreferences.join(', ')}.`);
+        }
+
+        setDatePreference(preference);
+    };
+
+    return [datePreference, setAndCheckTimePreference];
+};
+
+const useDatePreference = () => useStore(storeKeys.useDatePreference);
+
+export default useDatePreference;
