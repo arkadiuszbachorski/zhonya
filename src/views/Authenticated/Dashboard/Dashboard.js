cf68f7e90a17f02bbf0e341e5f5a4a8360b505f4
Arkadiusz Bachorski <quez2223@gmail.com>
2020-02-10 20:28:16 +0100

    Use CardDashboard component

--- src/views/Authenticated/Dashboard/Dashboard.js
+++ src/views/Authenticated/Dashboard/Dashboard.js
@@ -1,71 +1,85 @@
 import React, { useEffect } from 'react';
-import { useIntl, FormattedMessage } from 'react-intl';
-import { faCheckSquare, faClock, faUser, faTag, faList, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useIntl } from 'react-intl';
+import { faCheckSquare, faClock, faList, faPlus, faTag, faUser } from '@fortawesome/free-solid-svg-icons';
 import Container from '../../../components/Container/Container';
 import useAuthenticatedOnly from '../../../hooks/useAuthenticatedOnly';
 import PanelTemplate from '../../../components/PanelTemplate/PanelTemplate';
 import useInstanceWithErrorsAndToastsAndLoading from '../../../hooks/api/useInstanceWithErrorsAndToastsAndLoading';
-import Card from '../../../components/Card/Card';
-import PageTitle from '../../../components/typography/PageTitle/PageTitle';
-import ButtonRoundIcon from '../../../components/buttons/ButtonRoundIcon/ButtonRoundIcon';
 import routes from '../../../routes';
 import styles from './Dashboard.module.scss';
+import CardDashboard from './CardDashboard/CardDashboard';
 
 const Dashboard = () => {
     useAuthenticatedOnly();
 
     const [instance, loading, errors] = useInstanceWithErrorsAndToastsAndLoading();
 
-    const { formatMessage } = useIntl();
-
     useEffect(() => {
         //    todo: get dashboard data here
     }, []);
 
     return (
-        <PanelTemplate>
+        <PanelTemplate titleId="action.dashboard">
             <Container variant={['smallItems']} className={styles.wrapper}>
-                <Card>
-                    <PageTitle tag="h4" className={styles.cardTitle}>
-                        <FormattedMessage id="model.tag.plural" />{' '}
-                        <FontAwesomeIcon icon={faTag} className={styles.icon} />
-                    </PageTitle>
-                    <div className={styles.actionsWrapper}>
-                        <ButtonRoundIcon
-                            icon={faList}
-                            link
-                            size="small"
-                            to={routes.tag.index}
-                            title={formatMessage({ id: 'action.lists' })}
-                        />
-                        <ButtonRoundIcon
-                            icon={faPlus}
-                            link
-                            size="small"
-                            to={routes.tag.create}
-                            title={formatMessage({ id: 'action.create' })}
-                        />
-                        <ButtonRoundIcon icon={faSearch} size="small" title={formatMessage({ id: 'action.lists' })} />
-                    </div>
-                </Card>
-                <Card>
-                    <PageTitle tag="h4" className={styles.cardTitle}>
-                        <FormattedMessage id="model.task.plural" />{' '}
-                        <FontAwesomeIcon icon={faCheckSquare} className={styles.icon} />
-                    </PageTitle>
-                </Card>
-                <Card>
-                    <PageTitle tag="h4" className={styles.cardTitle}>
-                        <FormattedMessage id="model.attempt.plural" />{' '}
-                        <FontAwesomeIcon icon={faClock} className={styles.icon} />
-                    </PageTitle>
-                </Card>
-                <Card>
-                    <PageTitle tag="h4" className={styles.cardTitle}>
-                        <FormattedMessage id="model.user" /> <FontAwesomeIcon icon={faUser} className={styles.icon} />
-                    </PageTitle>
-                </Card>
+                <CardDashboard
+                    titleId="model.tag.plural"
+                    icon={faTag}
+                    buttons={[
+                        {
+                            icon: faList,
+                            to: routes.tag.index,
+                            titleId: 'action.list',
+                        },
+                        {
+                            icon: faPlus,
+                            to: routes.tag.create,
+                            titleId: 'action.create',
+                        },
+                    ]}
+                />
+                <CardDashboard
+                    titleId="model.task.plural"
+                    icon={faCheckSquare}
+                    buttons={[
+                        {
+                            icon: faList,
+                            to: routes.task.index,
+                            titleId: 'action.list',
+                        },
+                        {
+                            icon: faPlus,
+                            to: routes.task.create,
+                            titleId: 'action.create',
+                        },
+                    ]}
+                />
+                <CardDashboard
+                    titleId="model.attempt.plural"
+                    icon={faClock}
+                    buttons={[
+                        {
+                            icon: faList,
+                            to: routes.attempt.index,
+                            titleId: 'action.list',
+                        },
+                        {
+                            icon: faPlus,
+                            to: routes.attempt.create,
+                            titleId: 'action.create',
+                        },
+                    ]}
+                />
+                <CardDashboard
+                    titleId="model.user"
+                    icon={faUser}
+                    buttons={[
+                        {
+                            icon: faUser,
+                            to: routes.user.settings,
+                            titleId: 'action.settings',
+                        },
+                    ]}
+                />
             </Container>
         </PanelTemplate>
     );
