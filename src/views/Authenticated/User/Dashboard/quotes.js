02dd10c5cafd8a61f4d3c19d7b2061d5559d6cc1
Arkadiusz Bachorski <quez2223@gmail.com>
2020-03-05 09:13:42 +0100

    Add new quotes

--- src/views/Authenticated/User/Dashboard/quotes.js
+++ src/views/Authenticated/User/Dashboard/quotes.js
@@ -1,14 +1,96 @@
 export default {
     pl: [
         {
+            author: 'Benjamin Franklin',
+            content: 'Czas to pieniądz',
+        },
+        {
+            author: 'Ryum Jin',
+            content: ['Motywacja jest tym, co pozwala Ci zacząć.', 'Nawyk jest tym, co pozwala Ci wytrwać.'],
+        },
+        {
+            author: 'Brian Tracy',
+            content: 'Twój los zależy od Twoich nawyków',
+        },
+        {
+            author: 'Arystoteles',
+            content: [
+                'Jesteśmy tym, co w swoim życiu powtarzamy.',
+                'Doskonałość nie jest jednorazowym aktem, lecz nawykiem.',
+            ],
+        },
+        {
+            author: 'Mark Fisher',
+            content: [
+                'Ludzie, którzy tracą czas czekając, aż zaistnieją najbardziej sprzyjające warunki, nigdy nic nie zdziałają.',
+                'Najlepszy czas na działanie jest teraz',
+            ],
+        },
+        {
+            author: 'Arystoteles',
+            content: [
+                'Po pierwsze, miej zdefiniowany, jasno określony cel.',
+                'Po drugie, zdobądź potrzebne zasoby by do niego dojść; wiedzę, pieniądze, materiały, metody.',
+                'Po trzecie, skieruj wszystkie te zasoby na osiągnięcie celu.',
+            ],
+        },
+        {
+            author: 'Konfucjusz',
+            content: 'Tysiącmilowa podróż zaczyna się od pierwszego kroku',
+        },
+        {
+            author: 'Mark Twain',
+            content: 'Za dwadzieścia lat bardziej będziesz żałował tego, czego nie zrobiłeś, niż tego, co zrobiłeś',
+        },
+        {
             author: 'Steve Jobs',
-            content: 'Gdyby kózka nie skakała, to by ślimak lubił czas',
+            content: 'Twój czas jest ograniczony, a więc nie marnuj go na życie cudzym życiem',
+        },
+        {
+            author: 'Jim Rohn',
+            content: 'Zmień swoje 24 godziny, a zmienisz swoje życie',
         },
     ],
     en: [
         {
+            author: 'Benjamin Franklin',
+            content: 'Time is money',
+        },
+        {
+            author: 'William Penn',
+            content: 'Time is what we want most, but what we use worst',
+        },
+        {
+            author: 'Lao Tzu',
+            content: 'Time is a created thing. To say “I don’t have time” is to say “I don’t want to”.',
+        },
+        {
             author: 'Steve Jobs',
-            content: 'I like kitties',
+            content: 'It’s really clear that the most precious resource we all have is time',
+        },
+        {
+            author: 'William Shakespeare',
+            content: 'Better three hours to soon than one minute too late',
+        },
+        {
+            author: 'Benjamin Franklin',
+            content: 'Never leave ’till tomorrow which you can do today',
+        },
+        {
+            author: 'Andy Warhol',
+            content: 'They always say time changes things, but you actually have to change them yourself',
+        },
+        {
+            author: 'Charles Darwin',
+            content: 'A man who dares to waste one hour of time has not discovered the value of life',
+        },
+        {
+            author: 'Jim Rohn',
+            content: 'Either you run the day, or the day runs you',
+        },
+        {
+            author: 'Jim Rohn',
+            content: 'Change your 24 hours and you will change your life',
         },
     ],
 };
