901e08f1101f5d0dcf06b52fc4335c3697e9d1c8
Arkadiusz Bachorski <quez2223@gmail.com>
2020-03-05 14:41:03 +0100

    Init it on MainPage

--- src/views/Public/MainPage/MainPage.js
+++ src/views/Public/MainPage/MainPage.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import AnimatedClock from '../../../components/AnimatedClock/AnimatedClock';
 import Jumbotron from '../../../components/Jumbotron/Jumbotron';
 import routes from '../../../routes';
@@ -8,22 +10,33 @@ import ForWho from './ForWho/ForWho';
 import HowItWorks from './HowItWorks/HowItWorks';
 import Contact from './Contact/Contact';
 
-const MainPage = () => (
-    <MainTemplate animate>
-        <AnimatedClock />
-        <Jumbotron>
-            <Jumbotron.Title>
-                <FormattedMessage id="main.jumbotron.first" /> <br />
-                <FormattedMessage id="main.jumbotron.second" />
-            </Jumbotron.Title>
-            <Jumbotron.Action link to={routes.signUp} variant="primaryLight">
-                <FormattedMessage id="main.jumbotron.button" />
-            </Jumbotron.Action>
-        </Jumbotron>
-        <ForWho />
-        <HowItWorks />
-        <Contact />
-    </MainTemplate>
-);
+const MainPage = () => {
+    useEffect(() => {
+        AOS.init({
+            offset: 100,
+            duration: 500,
+            easing: 'ease-in-out',
+            once: true,
+        });
+    }, []);
+
+    return (
+        <MainTemplate animate>
+            <AnimatedClock />
+            <Jumbotron>
+                <Jumbotron.Title>
+                    <FormattedMessage id="main.jumbotron.first" /> <br />
+                    <FormattedMessage id="main.jumbotron.second" />
+                </Jumbotron.Title>
+                <Jumbotron.Action link to={routes.signUp} variant="primaryLight">
+                    <FormattedMessage id="main.jumbotron.button" />
+                </Jumbotron.Action>
+            </Jumbotron>
+            <ForWho />
+            <HowItWorks />
+            <Contact />
+        </MainTemplate>
+    );
+};
 
 export default MainPage;
