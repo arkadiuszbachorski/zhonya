e033feb1d0a490d18d2e4a97efc49986af6320e3
Arkadiusz Bachorski <quez2223@gmail.com>
2020-03-26 20:16:23 +0100

    Implement preparation mechanism

--- src/e2e/utils/manageUser.js
+++ src/e2e/utils/manageUser.js
@@ -1,27 +1,30 @@
 import axios from 'axios';
 import routes from '../../routes';
 
-const params = type => {
+const params = (type, preparation = undefined) => {
     const url = `${process.env.REACT_APP_API_URL}/e2e/${type}`;
     const data = {
         token: process.env.REACT_APP_E2E_TOKEN,
     };
+    if (preparation) {
+        data.preparation = preparation;
+    }
 
     return [url, data];
 };
 
-export const createUser = () => {
-    return axios.post(...params('create'));
+export const createUser = (preparation = undefined) => {
+    return axios.post(...params('create', preparation));
 };
 
 export const wipeUser = () => {
     return axios.post(...params('wipe'));
 };
 
-export const loginUser = async (page, credits) => {
+export const loginUser = async (page, data) => {
     await page.route(routes.logIn);
-    await page.typeInput('#email', credits.email);
-    await page.typeInput('#password', credits.password);
+    await page.typeInput('#email', data.email);
+    await page.typeInput('#password', data.password);
     await page.click('label[for="rememberMe"]');
     await page.clickSubmit();
     await page.waitForNavigation();
