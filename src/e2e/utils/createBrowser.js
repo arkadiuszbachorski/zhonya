e2d488eee99bd5e12be7a95f04d3f1c48bfa8bbd
Arkadiusz Bachorski <quez2223@gmail.com>
2020-03-26 15:52:54 +0100

    Check env value with identify operator

--- src/e2e/utils/createBrowser.js
+++ src/e2e/utils/createBrowser.js
@@ -1,15 +1,15 @@
 import puppeteer from 'puppeteer';
 
-const defaultOptions = {
-    args: ['--lang=en-EN'],
-};
-
-if (!process.env.REACT_APP_TEST_HEADLESS) {
-    defaultOptions.headless = false;
-    defaultOptions.slowMo = 15;
-}
-
 export default function createBrowser(options) {
+    const defaultOptions = {
+        args: ['--lang=en-EN'],
+    };
+
+    if (process.env.REACT_APP_TEST_HEADLESS === 'false') {
+        defaultOptions.headless = false;
+        defaultOptions.slowMo = 30;
+    }
+
     return puppeteer.launch({
         ...defaultOptions,
         ...options,
